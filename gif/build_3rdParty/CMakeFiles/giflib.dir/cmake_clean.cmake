7d324e5a5ebc54c3aff7bada1dc5b92fddda7ac2
HP_3500 <sergeymaksimov78@yandex.ru>
2025-01-20 06:30:41 +0700

    clear CMake directory

--- gif/build_3rdParty/CMakeFiles/giflib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libgiflib.a"
-  "libgiflib.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/giflib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
